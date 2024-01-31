1d9f633c5fe27f40aa40a70ac51bd2726569849a
AJEESH-MJ <ajeeshjayanm@gmail.com>
2024-01-31 16:32:23 +0530

    Routes Updated based on the public url

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import { Footer, Navbar } from './components/index';
 import { About, Contact, Homepage, SignIn, SignUp } from './pages';
 
 function App() {
+  const homeRoute = process.env.PUBLIC_URL;
+
   return (
     <div className="App">
-     <Navbar />
-     <Routes>
-        <Route path="/MYPERRO/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+      <Navbar />
+      <Routes>
+        <Route path={`${homeRoute}/`} element={<Homepage />} />
+        <Route path={`${homeRoute}/about`} element={<About />} />
+        <Route path={`${homeRoute}/contact`} element={<Contact />} />
+        <Route path={`${homeRoute}/signin`} element={<SignIn />} />
+        <Route path={`${homeRoute}/signup`} element={<SignUp />} />
       </Routes>
       <Footer />
     </div>
   );
 }
 
-export default App; 
+export default App;
